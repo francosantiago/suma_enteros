0e39edbeb9ca20dc5e3fe41bb86aeb6828083b17
Santiago Darío Franco Ruiz <102566762+francosantiago@users.noreply.github.com>
2023-02-18 11:01:38 -0500

    Create Test.java

---
+++ ejecutable/Test.java
@@ -0,0 +1,31 @@
+package ejecutable;
+
+import javax.swing.JOptionPane;
+
+public class Test
+{
+    public static void main(String[] args) 
+    {   
+        /*declaración
+        de
+        variables
+        */
+
+        int x;
+        int y;
+        int z;
+
+        //input
+        
+        x = Integer.parseInt(JOptionPane.showInputDialog("Digite el valor de x: "));
+        y = Integer.parseInt(JOptionPane.showInputDialog("Digite el valor de y: "));
+
+        //processing
+
+        z = x +y;
+
+        // output
+
+        JOptionPane.showMessageDialog(null, "La suma de los dos enteros es = " + z);
+    }
+}
